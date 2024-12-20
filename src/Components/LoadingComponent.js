87082490be0b25ddc1fd75ae01b13c518b046a5c
haroon175 <haroonahmadpk500@gmail.com>
2024-12-20 17:34:10 +0500

    loading

--- src/Components/LoadingComponent.js
+++ src/Components/LoadingComponent.js
@@ -1,38 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { Container, CircularProgress, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import './Loading.css';
 
 const LoadingComponent = () => {
-  const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 5000); 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 5000); 
+        return () => clearTimeout(timer);
+    }, []);
 
-    return () => clearTimeout(timer);
-  }, []);
+    if (!loading) {
+        return null; 
+    }
 
-  if (!loading) {
-    
-    return null; 
-  }
+    return (
+        <Container
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
 
-  return (
-    <Container
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <CircularProgress sx={{ color: '#950101' }} />
-      <Typography variant="h6" sx={{ marginTop: 2, color: '#950101', fontWeight:'bold' }}>
-        NestFllix...
-      </Typography>
-    </Container>
-  );
+            }}
+        >
+            <div id="load">
+                
+                <div>X</div>
+                <div>I</div>
+                <div>L</div>
+                <div>F</div>
+                <div>S</div>
+                <div>E</div>
+                <div>N</div>
+                <div>T</div>
+            </div>
+
+            <Typography
+                variant="h6"
+                sx={{ marginTop: 2, color: '#950101', fontWeight: 'bold' }}
+            >
+                Loading...
+            </Typography>
+        </Container>
+    );
 };
 
 export default LoadingComponent;
